9deb5320b410490ca65bd3c215df730b47b17a10
HaiwenZhang <haiwensummer@gmail.com>
2023-01-02 15:36:07 +0800

    Init Math201 LaTeX Template

---
+++ code/lab01.py
@@ -0,0 +1,20 @@
+import math
+
+def caculate(x):
+    return 2 * x * math.cos(x)**2 + math.pi/2 - math.sin(x) * math.cos(x) - math.pi/4
+
+def main():
+    r = 10
+    x = 0
+    x0 = caculate(x)
+
+    while abs(x0 - x) >= 0.00001:
+        x = x0;
+        x0 = caculate(x)
+    R = 2 * 10 * math.cos(x0)
+
+    print("x0 = ", x0)
+    print("R = ", R)
+
+if __name__ == "__main__":
+    main()
